ade853bc45a7fc879a5d2f21847255d764867cda
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-02-19 20:24:41 +0000

    Added sample todo-darwin-umbrella module

---
+++ sample/todo-darwin-umbrella/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+project.setupMultiplatform()
+
+kotlin {
+    iosX64().setupIosBinaries()
+    iosArm64().setupIosBinaries()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(project(":mvikotlin"))
+                api(project(":logging"))
+                api(project(":timetravel"))
+                api(project(":sample:todo-common"))
+                api(project(":sample:todo-reaktive"))
+            }
+        }
+    }
+}
+
+fun KotlinNativeTarget.setupIosBinaries() {
+    binaries {
+        framework {
+            baseName = "TodoLib"
+//                freeCompilerArgs = freeCompilerArgs.plus("-Xobjc-generics")
+
+            export(project(":mvikotlin"))
+            export(project(":logging"))
+            export(project(":timetravel"))
+            export(project(":sample:todo-common"))
+            export(project(":sample:todo-reaktive"))
+        }
+    }
+}
