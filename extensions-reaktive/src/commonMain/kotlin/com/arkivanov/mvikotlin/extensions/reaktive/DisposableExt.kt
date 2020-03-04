b7d0a2b662a51c34704047485e178d3e4fc4287e
Arkadii Ivanov <arkann1985@gmail.com>
2020-03-04 22:16:23 +0000

    Made Disposable.toReaktiveDisposable extension internal

--- extensions-reaktive/src/commonMain/kotlin/com/arkivanov/mvikotlin/extensions/reaktive/DisposableExt.kt
+++ extensions-reaktive/src/commonMain/kotlin/com/arkivanov/mvikotlin/extensions/reaktive/DisposableExt.kt
@@ -3,7 +3,7 @@ package com.arkivanov.mvikotlin.extensions.reaktive
 import com.arkivanov.mvikotlin.core.rx.Disposable
 import com.badoo.reaktive.disposable.Disposable as ReaktiveDisposable
 
-fun Disposable.toReaktiveDisposable(): ReaktiveDisposable =
+internal fun Disposable.toReaktiveDisposable(): ReaktiveDisposable =
     object : ReaktiveDisposable {
         override val isDisposed: Boolean
             get() = this@toReaktiveDisposable.isDisposed
