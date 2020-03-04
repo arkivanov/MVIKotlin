f95d2fc0cbbc5ab629be1dfc05cdb7cb26099c9a
Arkadii Ivanov <arkann1985@gmail.com>
2020-03-04 22:11:58 +0000

    Fixed formatting in View

--- core/src/commonMain/kotlin/com/arkivanov/mvikotlin/core/view/View.kt
+++ core/src/commonMain/kotlin/com/arkivanov/mvikotlin/core/view/View.kt
@@ -1,3 +1,3 @@
 package com.arkivanov.mvikotlin.core.view
 
-interface View<in Model: Any, out Event : Any> : ViewRenderer<Model>, ViewEvents<Event>
+interface View<in Model : Any, out Event : Any> : ViewRenderer<Model>, ViewEvents<Event>
