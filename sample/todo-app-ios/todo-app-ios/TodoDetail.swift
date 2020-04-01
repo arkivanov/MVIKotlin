8183706482dcf71bae7185b3b032c08491329211
Sebastian Gallese <sebastiangallese@gmail.com>
2020-03-31 23:22:02 -0700

    Add skeleton TodoDetail view

---
+++ sample/todo-app-ios/todo-app-ios/TodoDetail.swift
@@ -0,0 +1,31 @@
+//
+//  TodoDetail.swift
+//  todo-app-ios
+//
+//  Created by Arkadii Ivanov on 3/31/20.
+//  Copyright © 2020 arkivanov. All rights reserved.
+//
+
+import SwiftUI
+import TodoLib
+
+struct TodoDetail: View {
+    @ObservedObject var listView = TodoListViewImpl()
+    
+    var body: some View {
+        List(listView.model?.items ?? []) { item in
+            TodoRow(
+                text: item.data.text,
+                isDone: item.data.isDone,
+                onDoneClicked: { self.listView.dispatch(event: TodoListViewEvent.ItemDoneClicked(id: item.id)) },
+                onDeleteClicked: { self.listView.dispatch(event: TodoListViewEvent.ItemDeleteClicked(id: item.id)) }
+            )
+        }
+    }
+}
+
+struct TodoDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoList()
+    }
+}
