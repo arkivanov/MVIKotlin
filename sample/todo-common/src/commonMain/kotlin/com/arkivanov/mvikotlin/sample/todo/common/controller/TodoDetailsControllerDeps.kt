0fc31205098cd241470fa34566e9097be8b188c1
Sebastian Gallese <sebastiangallese@gmail.com>
2020-04-08 22:51:51 -0700

    Add lifecycle to details deps

--- sample/todo-common/src/commonMain/kotlin/com/arkivanov/mvikotlin/sample/todo/common/controller/TodoDetailsControllerDeps.kt
+++ sample/todo-common/src/commonMain/kotlin/com/arkivanov/mvikotlin/sample/todo/common/controller/TodoDetailsControllerDeps.kt
@@ -1,11 +1,12 @@
 package com.arkivanov.mvikotlin.sample.todo.common.controller
 
+import com.arkivanov.mvikotlin.core.lifecycle.Lifecycle
 import com.arkivanov.mvikotlin.core.store.StoreFactory
-import com.arkivanov.mvikotlin.core.utils.statekeeper.StateKeeperProvider
 import com.arkivanov.mvikotlin.sample.todo.common.database.TodoDatabase
 
 class TodoDetailsControllerDeps(
     override val storeFactory: StoreFactory,
     override val database: TodoDatabase,
+    override val lifecycle: Lifecycle,
     override val itemId: String
 ) : TodoDetailsController.Dependencies
