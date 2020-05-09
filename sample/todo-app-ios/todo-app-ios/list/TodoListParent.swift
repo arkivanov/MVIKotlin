1004e9e30c1572d9e35db0de76118479c509428f
Arkadii Ivanov <arkann1985@gmail.com>
2020-05-09 16:29:39 -0700

    Fix iOS app

--- sample/todo-app-ios/todo-app-ios/list/TodoListParent.swift
+++ sample/todo-app-ios/todo-app-ios/list/TodoListParent.swift
@@ -10,12 +10,12 @@ import SwiftUI
 import TodoLib
 
 struct TodoListParent: View {
-
+    
     var controllerDeps: ControllerDeps
     
     var body: some View {
         let lifecycle = LifecycleWrapper()
-
+        
         let controller = TodoListReaktiveController(
             dependencies: TodoListControllerDeps(
                 storeFactory: controllerDeps.storeFactory,
@@ -23,13 +23,13 @@ struct TodoListParent: View {
                 lifecycle: lifecycle.lifecycle
             )
         )
-
+        
         let todoList = TodoList<TodoDetailsParent>(details: { id in TodoDetailsParent(id: id, controllerDeps: self.controllerDeps) })
         let todoAdd = TodoAdd()
-
+        
         let lv = todoList.listView
         let av = todoAdd.addView
-        controller.onViewCreated(todoListView: lv, todoAddView: av, viewLifecycle: lifecycle.lifecycle)
+        controller.onViewCreated(todoListView: lv, todoAddView: av, viewLifecycle: lifecycle.lifecycle, output: handleOutput)
         
         let todoListViews = VStack {
             todoList
@@ -39,5 +39,13 @@ struct TodoListParent: View {
         return todoListViews
             .onAppear(perform: lifecycle.start)
             .onDisappear(perform: lifecycle.stop)
+    }
+    
+    private func handleOutput(output: TodoListControllerOutput) {
+        switch (output) {
+        case is TodoListControllerOutput.ItemSelected:
+        break // Could not find a way to push the details view programmatically, so it's handled by the NavigationLink in TodoList
+        default: break
+        }
     }
 }
