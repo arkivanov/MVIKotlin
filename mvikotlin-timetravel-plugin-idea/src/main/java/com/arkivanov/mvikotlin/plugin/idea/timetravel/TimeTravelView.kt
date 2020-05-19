386c0b29bbc39232a31490afdf5c374ede57409a
Arkadii Ivanov <arkann1985@gmail.com>
2020-05-19 22:47:33 +0100

    Fix recursive view update in plugin

--- mvikotlin-timetravel-plugin-idea/src/main/java/com/arkivanov/mvikotlin/plugin/idea/timetravel/TimeTravelView.kt
+++ mvikotlin-timetravel-plugin-idea/src/main/java/com/arkivanov/mvikotlin/plugin/idea/timetravel/TimeTravelView.kt
@@ -7,15 +7,10 @@ import com.arkivanov.mvikotlin.timetravel.proto.internal.value.ValueTextTreeBuil
 import com.intellij.ui.JBSplitter
 import com.intellij.ui.components.JBList
 import com.intellij.ui.components.JBScrollPane
-import org.jdesktop.swingx.renderer.CellContext
-import org.jdesktop.swingx.renderer.ComponentProvider
 import org.jdesktop.swingx.renderer.DefaultListRenderer
-import org.jdesktop.swingx.renderer.JRendererLabel
 import java.awt.BorderLayout
-import java.awt.Font
 import javax.swing.DefaultListModel
 import javax.swing.JComponent
-import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTree
 import javax.swing.tree.DefaultMutableTreeNode
@@ -25,36 +20,20 @@ internal class TimeTravelView(
     private val listener: Listener
 ) {
 
-    private var state = State()
-
     private val toolbar = TimeTravelToolbar(listener, ::onDebug)
     private val listModel = DefaultListModel<TimeTravelEvent>()
 
     private val list =
         JBList(listModel).apply {
-            cellRenderer = DefaultListRenderer(
-                object : ComponentProvider<JLabel>() {
-                    override fun createRendererComponent(): JLabel = JRendererLabel()
-
-                    override fun configureState(cellContext: CellContext) {
-                        (rendererComponent as JLabel).horizontalAlignment = horizontalAlignment
-                    }
-
-                    override fun format(cellContext: CellContext) {
-                        val label = rendererComponent as JLabel
-                        val event = cellContext.value as TimeTravelEvent
-                        label.text = event.description
-                        label.font = font.deriveFont(if (state.selectedEventIndex == cellContext.row) Font.BOLD else Font.PLAIN)
-                    }
-                }
-            )
-
             addListSelectionListener { refreshUi() }
         }
 
     private val treeBuilder = ValueTextTreeBuilder(newNode = ::DefaultMutableTreeNode, addChild = DefaultMutableTreeNode::add)
     private val treeModel = DefaultTreeModel(null)
     private val tree = JTree(treeModel)
+    private var isUpdatingUi = false
+    private var isUpdatingEvents = false
+    private var isRefreshingUi = false
 
     val content: JComponent =
         JPanel(BorderLayout()).apply {
@@ -74,20 +53,43 @@ internal class TimeTravelView(
     }
 
     fun render(state: State, eventsUpdate: TimeTravelEventsUpdate? = null) {
-        this.state = state
-        eventsUpdate?.also(::onEventsUpdate)
+        updateUi(state)
+        eventsUpdate?.also(::updateEvents)
         refreshUi()
     }
 
-    private fun onEventsUpdate(eventsUpdate: TimeTravelEventsUpdate) {
-        when (eventsUpdate) {
-            is TimeTravelEventsUpdate.All -> {
-                listModel.clear()
-                addEvents(eventsUpdate.events)
-            }
+    private fun updateUi(state: State) {
+        isUpdatingUi = true
+        try {
+            toolbar.setConnectionStatus(state.connectionStatus)
+            toolbar.setMode(state.mode)
 
-            is TimeTravelEventsUpdate.New -> addEvents(eventsUpdate.events)
-        }.let {}
+            list.cellRenderer =
+                DefaultListRenderer(
+                    TimeTravelEventComponentProvider(
+                        font = list.font,
+                        selectedEventIndex = state.selectedEventIndex
+                    )
+                )
+        } finally {
+            isUpdatingUi = false
+        }
+    }
+
+    private fun updateEvents(eventsUpdate: TimeTravelEventsUpdate) {
+        isUpdatingEvents = true
+        try {
+            when (eventsUpdate) {
+                is TimeTravelEventsUpdate.All -> {
+                    listModel.clear()
+                    addEvents(eventsUpdate.events)
+                }
+
+                is TimeTravelEventsUpdate.New -> addEvents(eventsUpdate.events)
+            }.let {}
+        } finally {
+            isUpdatingEvents = false
+        }
     }
 
     private fun addEvents(events: Iterable<TimeTravelEvent>) {
@@ -95,9 +97,18 @@ internal class TimeTravelView(
     }
 
     private fun refreshUi() {
-        toolbar.update(state.connectionStatus, state.mode, list.selectedValue?.type?.isDebuggable == true)
-        list.updateUI()
-        treeModel.setRoot(list.selectedValue?.value?.let(treeBuilder::build))
+        if (isUpdatingUi || isUpdatingEvents || isRefreshingUi) {
+            return
+        }
+
+        isRefreshingUi = true
+        try {
+            toolbar.setDebugEnabled(list.selectedValue?.type?.isDebuggable == true)
+            list.updateUI()
+            treeModel.setRoot(list.selectedValue?.value?.let(treeBuilder::build))
+        } finally {
+            isRefreshingUi = false
+        }
     }
 
     private fun onDebug() {
