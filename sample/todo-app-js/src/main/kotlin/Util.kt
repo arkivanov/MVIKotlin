32f35e5b4c00d6123985b656b2f461cecb3000d4
max <kellia-16@yandex.ru>
2020-05-23 12:04:40 +0300

    remove unused class and params add build command into pipeline script

--- sample/todo-app-js/src/main/kotlin/Util.kt
+++ sample/todo-app-js/src/main/kotlin/Util.kt
@@ -1,10 +1,6 @@
-
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.logging.store.LoggingStoreFactory
 import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
-
-const val ANIMATION_DURATION: Int = 200
-const val DEBUG: Boolean = true
 
 val storeFactoryInstance: StoreFactory = LoggingStoreFactory(delegate = DefaultStoreFactory)
 
