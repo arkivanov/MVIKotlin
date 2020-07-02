b766910f3f6f0f62e8bc7a4050a34778f28da562
Arkadii Ivanov <arkann1985@gmail.com>
2020-07-02 18:02:31 +0100

    Updated logging tests

---
+++ mvikotlin-logging/src/commonTest/kotlin/com/arkivanov/mvikotlin/logging/logger/DefaultLogFormatterTest.kt
@@ -0,0 +1,54 @@
+package com.arkivanov.mvikotlin.logging.logger
+
+import com.arkivanov.mvikotlin.core.store.StoreEventType
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class DefaultLogFormatterTest {
+
+    @Test
+    fun result_contains_whole_value_WHEN_valueLengthLimit_not_exceeded() {
+        val formatter = DefaultLogFormatter(valueLengthLimit = 10)
+        val value = "abcdefghij"
+
+        val result = formatter.format(storeName = "store", eventType = StoreEventType.STATE, value = value)
+
+        assertNotNull(result)
+        assertTrue(result.contains(value))
+    }
+
+    @Test
+    fun result_contains_truncated_value_WHEN_valueLengthLimit_exceeded() {
+        val formatter = DefaultLogFormatter(valueLengthLimit = 10)
+        val value = "abcdefghijk"
+
+        val result = formatter.format(storeName = "store", eventType = StoreEventType.STATE, value = value)
+
+        assertNotNull(result)
+        assertTrue(result.contains(value.take(10)))
+    }
+
+    @Test
+    fun result_does_not_contain_whole_value_WHEN_valueLengthLimit_exceeded() {
+        val formatter = DefaultLogFormatter(valueLengthLimit = 10)
+        val value = "abcdefghijk"
+
+        val result = formatter.format(storeName = "store", eventType = StoreEventType.STATE, value = value)
+
+        assertNotNull(result)
+        assertFalse(result.contains(value))
+    }
+
+    @Test
+    fun result_does_not_contain_value_WHEN_valueLengthLimit_is_0() {
+        val formatter = DefaultLogFormatter(valueLengthLimit = 0)
+        val value = "12345"
+
+        val result = formatter.format(storeName = "store", eventType = StoreEventType.STATE, value = value)
+
+        assertNotNull(result)
+        assertFalse(result.any { it in value })
+    }
+}
--- mvikotlin-logging/src/commonTest/kotlin/com/arkivanov/mvikotlin/logging/store/LoggingStoreFactoryTest.kt
+++ mvikotlin-logging/src/commonTest/kotlin/com/arkivanov/mvikotlin/logging/store/LoggingStoreFactoryTest.kt
@@ -4,10 +4,12 @@ import com.arkivanov.mvikotlin.core.store.Bootstrapper
 import com.arkivanov.mvikotlin.core.store.Executor
 import com.arkivanov.mvikotlin.core.store.Reducer
 import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreEventType
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.core.test.internal.TestBootstrapper
 import com.arkivanov.mvikotlin.core.test.internal.TestExecutor
 import com.arkivanov.mvikotlin.core.test.internal.reducer
+import com.arkivanov.mvikotlin.logging.logger.LogFormatter
 import com.arkivanov.mvikotlin.logging.logger.Logger
 import com.arkivanov.mvikotlin.rx.Disposable
 import com.arkivanov.mvikotlin.rx.Observer
@@ -18,72 +20,74 @@ import com.badoo.reaktive.utils.atomic.update
 import com.badoo.reaktive.utils.freeze
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class LoggingStoreFactoryTest {
 
-    private val logger = TestLogger()
+    private val formatter = TestLogFormatter()
+    private val logger = TestLogger(formatter)
 
     @Test
-    fun logs_creation() {
-        store(logger = logger)
+    fun logs_created() {
+        store()
 
-        logger.assertSubString("created")
+        logger.assertLoggedText("$STORE_NAME: created")
     }
 
     @Test
-    fun logs_disposal() {
-        store(logger = logger).dispose()
+    fun logs_disposed() {
+        store().dispose()
 
-        logger.assertSubString("disposed")
+        logger.assertLoggedText("$STORE_NAME: disposed")
     }
 
     @Test
     fun logs_intents() {
-        val store = store(logger = logger)
+        val store = store()
 
         store.accept("some_intent")
 
-        logger.assertSubString("some_intent")
+        logger.assertLoggedEvent(StoreEventType.INTENT, "some_intent")
     }
 
     @Test
     fun logs_actions() {
         val bootstrapper = TestBootstrapper()
-        store(bootstrapper = bootstrapper, logger = logger)
+        store(bootstrapper = bootstrapper)
 
         bootstrapper.dispatch("some_action")
 
-        logger.assertSubString("some_action")
+        logger.assertLoggedEvent(StoreEventType.ACTION, "some_action")
     }
 
     @Test
     fun logs_results() {
         val executor = TestExecutor()
-        store(executorFactory = { executor }, reducer = reducer { "new_state" }, logger = logger)
+        store(executorFactory = { executor }, reducer = reducer { "new_state" })
 
         executor.dispatch("some_result")
 
-        logger.assertSubString("some_result")
+        logger.assertLoggedEvent(StoreEventType.RESULT, "some_result")
     }
 
     @Test
     fun logs_labels() {
         val executor = TestExecutor()
-        store(executorFactory = { executor }, logger = logger)
+        store(executorFactory = { executor })
 
         executor.publish("some_label")
 
-        logger.assertSubString("some_label")
+        logger.assertLoggedEvent(StoreEventType.LABEL, "some_label")
     }
 
     @Test
     fun logs_states() {
         val executor = TestExecutor()
-        store(initialState = "initial_state", executorFactory = { executor }, reducer = reducer { "${this}_$it" }, logger = logger)
+        store(initialState = "initial_state", executorFactory = { executor }, reducer = reducer { "${this}_$it" })
 
         executor.dispatch("some_result")
 
-        logger.assertSubString("initial_state_some_result")
+        logger.assertLoggedEvent(StoreEventType.STATE, "initial_state_some_result")
     }
 
     @Test
@@ -97,23 +101,22 @@ class LoggingStoreFactoryTest {
                 initialState = "initial_state",
                 bootstrapper = bootstrapper,
                 executorFactory = { executor },
-                reducer = reducer { this },
-                logger = logger
+                reducer = reducer { this }
             )
 
         bootstrapper.dispatch("action")
         store.accept("intent")
+        store.dispose()
 
         logger.assertNoLogs()
     }
 
     private fun store(
-        name: String? = "name",
+        name: String? = STORE_NAME,
         initialState: String = "initial",
         bootstrapper: Bootstrapper<String>? = null,
         executorFactory: () -> Executor<String, String, String, String, String> = { TestExecutor() },
-        reducer: Reducer<String, String> = reducer(),
-        logger: Logger
+        reducer: Reducer<String, String> = reducer()
     ): Store<String, String, String> {
         val delegate =
             object : StoreFactory {
@@ -132,7 +135,7 @@ class LoggingStoreFactoryTest {
                     )
             }
 
-        val factory = LoggingStoreFactory(delegate = delegate, logger = logger)
+        val factory = LoggingStoreFactory(delegate = delegate, logger = logger, logFormatter = formatter)
 
         return factory.create(
             name = name,
@@ -143,20 +146,35 @@ class LoggingStoreFactoryTest {
         )
     }
 
-    private class TestLogger : Logger {
+    private companion object {
+        private const val STORE_NAME = "store"
+    }
+
+    private class TestLogger(
+        private val formatter: LogFormatter
+    ) : Logger {
         private val logs = AtomicList<String>()
 
         override fun log(text: String) {
             logs += text
         }
 
-        fun assertSubString(text: String) {
-            assertEquals(1, logs.value.count { it.contains(text) })
+        fun assertLoggedText(text: String) {
+            assertTrue(text in logs.value)
+        }
+
+        fun assertLoggedEvent(eventType: StoreEventType, value: Any?) {
+            assertLoggedText(requireNotNull(formatter.format(storeName = STORE_NAME, eventType = eventType, value = value)))
         }
 
         fun assertNoLogs() {
             assertEquals(emptyList(), logs.value)
         }
+    }
+
+    private class TestLogFormatter : LogFormatter {
+        override fun format(storeName: String, eventType: StoreEventType, value: Any?): String? =
+            "$storeName;$eventType;$value"
     }
 
     private class TestStore<in Intent : Any, Action : Any, out State : Any, Result : Any, Label : Any>(
