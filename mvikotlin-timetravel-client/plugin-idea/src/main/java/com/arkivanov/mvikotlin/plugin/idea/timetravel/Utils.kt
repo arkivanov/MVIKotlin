688cdd38a320b3b373b5e28a06bd8f858687816c
Arkadii Ivanov <arkann1985@gmail.com>
2020-07-13 19:59:16 +0100

    Better error messages and logging in IDEA plugin

--- mvikotlin-timetravel-client/plugin-idea/src/main/java/com/arkivanov/mvikotlin/plugin/idea/timetravel/Utils.kt
+++ mvikotlin-timetravel-client/plugin-idea/src/main/java/com/arkivanov/mvikotlin/plugin/idea/timetravel/Utils.kt
@@ -1,5 +1,6 @@
 package com.arkivanov.mvikotlin.plugin.idea.timetravel
 
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.Messages
 import java.io.BufferedReader
 import java.io.IOException
@@ -7,10 +8,18 @@ import java.io.IOException
 internal fun exec(params: List<String>): Process = Runtime.getRuntime().exec(params.toTypedArray())
 
 @Throws(IOException::class)
-internal fun Process.readError(): String? = errorStream?.bufferedReader()?.use(BufferedReader::readText)
+internal fun Process.readError(): String? =
+    errorStream
+        ?.bufferedReader()
+        ?.use(BufferedReader::readText)
+        ?.trim()
 
-internal fun log(text: String) {
-    println("[MVIKotlin]: $text")
+internal fun logI(text: String) {
+    Logger.getInstance("MVIKotlin").info(text)
+}
+
+internal fun logE(text: String, e: Throwable? = null) {
+    Logger.getInstance("MVIKotlin").error(text, e)
 }
 
 internal fun showErrorDialog(text: String) {
