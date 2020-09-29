1c063705051dca7abb2e2818d1028f673160fed7
Arkadii Ivanov <arkann1985@gmail.com>
2020-09-29 21:48:50 +0100

    Move Lifecycle extensions to mvikotlin module

--- mvikotlin/src/androidMain/kotlin/com/arkivanov/mvikotlin/core/lifecycle/AndroidLifecycleExt.kt
+++ mvikotlin/src/androidMain/kotlin/com/arkivanov/mvikotlin/core/lifecycle/AndroidLifecycleExt.kt
@@ -1,14 +1,9 @@
-package com.arkivanov.mvikotlin.extensions.androidx.lifecycle
+package com.arkivanov.mvikotlin.core.lifecycle
 
-import com.arkivanov.mvikotlin.core.lifecycle.Lifecycle
 import androidx.lifecycle.Lifecycle as AndroidLifecycle
 
 /**
  * Converts Androidx [Lifecycle][AndroidLifecycle] to MviKotlin [Lifecycle].
  * Requires [Java 1.8 source and target compatibility](https://developer.android.com/studio/write/java8-support).
  */
-@Deprecated(
-    "Use asMviLifecycle() from 'mvikotlin' module",
-    ReplaceWith("asMviLifecycle()", "com.arkivanov.mvikotlin.core.lifecycle.asMviLifecycle")
-)
 fun AndroidLifecycle.asMviLifecycle(): Lifecycle = AndroidLifecycleAdapter(this)
