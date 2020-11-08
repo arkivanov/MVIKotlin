cb07889a44d65a51957614b74e670641ec1ef4df
Arkadii Ivanov <arkann1985@gmail.com>
2020-11-08 12:58:01 +0000

    Introduce detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    id("io.gitlab.arturbosch.detekt").version("1.14.2")
 }
 
 allprojects {
@@ -7,5 +8,19 @@ allprojects {
         google()
         jcenter()
         maven("https://dl.bintray.com/badoo/maven")
+    }
+
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+        toolVersion = "1.14.2"
+        config = files("$rootDir/detekt.yml")
+
+        input =
+            files(
+                file("src")
+                    .listFiles()
+                    ?.filter { it.isDirectory && it.name.endsWith("main", ignoreCase = true) }
+            )
     }
 }
