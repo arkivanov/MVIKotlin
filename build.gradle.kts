42ec3dfbbe96d63f5cd84f50d616fe302ea4c832
Arkadii Ivanov <arkann1985@gmail.com>
2020-12-19 19:30:19 +0000

    Update Reaktive to 1.1.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ allprojects {
     repositories {
         google()
         jcenter()
-        maven("https://dl.bintray.com/badoo/maven")
     }
 
     apply(plugin = "io.gitlab.arturbosch.detekt")
