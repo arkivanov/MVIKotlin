835025a378ed3e3225fe7141f0c3d9925da86ee3
Arkadii Ivanov <arkann1985@gmail.com>
2021-10-03 15:06:10 +0100

    Updated AGP to 7.0.1 and gradle-setup-plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.toString() == "com.arkivanov.gradle.setup") {
-                useModule("com.github.arkivanov:gradle-setup-plugin:dbc19c4d71")
+                useModule("com.github.arkivanov:gradle-setup-plugin:29d636a815")
             }
         }
     }
