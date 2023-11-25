b99bf23f7a32d2e87351040fdc04ecb76ed95f07
Arkadii Ivanov <arkann1985@gmail.com>
2023-11-25 12:15:15 +0000

    Deleted all deprecated code

--- keepers/src/commonMain/kotlin/com/arkivanov/mvikotlin/keepers/statekeeper/ExperimentalStateKeeperApi.kt
+++
@@ -1,6 +0,0 @@
-package com.arkivanov.mvikotlin.keepers.statekeeper
-
-@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
-@Retention(AnnotationRetention.BINARY)
-@Deprecated(message = "This API is now provided by Essenty library: github.com/arkivanov/Essenty")
-annotation class ExperimentalStateKeeperApi
