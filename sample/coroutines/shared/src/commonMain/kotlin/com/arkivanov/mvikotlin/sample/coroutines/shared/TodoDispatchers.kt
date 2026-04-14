067f867b381a97680faf67982196c2c833231b22
Arkadii Ivanov <arkann1985@gmail.com>
2026-04-14 08:57:49 +0100

    Test CI

--- sample/coroutines/shared/src/commonMain/kotlin/com/arkivanov/mvikotlin/sample/coroutines/shared/TodoDispatchers.kt
+++ sample/coroutines/shared/src/commonMain/kotlin/com/arkivanov/mvikotlin/sample/coroutines/shared/TodoDispatchers.kt
@@ -3,7 +3,6 @@ package com.arkivanov.mvikotlin.sample.coroutines.shared
 import kotlinx.coroutines.CoroutineDispatcher
 
 interface TodoDispatchers {
-
     val main: CoroutineDispatcher
     val io: CoroutineDispatcher
     val unconfined: CoroutineDispatcher
